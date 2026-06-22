708341e0af63d63929412aa62debcd376a7c1b4c
user <lionelmombrin@gmail.com>
2026-06-22 11:59:22 +0200

    fichier main.py du service-agent avec tout les class et route et le fichier requirment avec tout les import nécéssaire

--- service-agent/main.py
+++ service-agent/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 from fastapi import FastAPI
-from sqlmodel import SQLModel, create_engine, Session ,Field
+from sqlmodel import SQLModel, create_engine, Session ,Field ,select
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -13,7 +13,7 @@ class Operation(SQLModel, table=True):
     compte_dest   : Optional[int] = Field(default=None)
     type_op       : str           = Field(default=None)
     montant       : float    = Field(default=None)
-    validee_par   : Optional[int] = Field(default=None)
+    traite_par   : Optional[int] = Field(default=None)
     statut        : str           = Field(default="en_attente")
     created_at    : datetime      = Field(default_factory=datetime.now)
 
@@ -24,6 +24,11 @@ class Compte(SQLModel, table=True):
     solde              :  float    = Field(default=None)
     derniere_operation : Optional[datetime] = Field(default=None)
 
+class User(SQLModel, table=True):
+    id    : Optional[int] = Field(default=None, primary_key=True)
+    email : str
+    role  : str
+
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
@@ -31,4 +36,82 @@ app = FastAPI()
 @app.get("/")
 def health_check():
     return {"status": "service-agent ok"}
+#fonctions sur les opération
+@app.get("/agent/operations/en-attente")
+def get_en_attente():
+    with Session(engine) as session:
+        operations = session.exec(
+            select(Operation).where(Operation.statut == "en_attente")
+        ).all()
+        return operations
 
+@app.get("/agent/operations/historique")
+def get_historique():
+    with Session(engine) as session:
+        operations = session.exec(
+            select(Operation).where(Operation.statut != "en_attente")
+        ).all()
+        return operations
+@app.patch("/agent/operations/{id}/valider")
+def valider_operation(id: int):
+    with Session(engine) as session:
+        # 1. Récupérer l'opération
+        operation = session.get(Operation, id)
+
+        # 2. Vérifier qu'elle existe
+        if not operation:
+            return {"error": "opération introuvable"}
+
+        operation.statut = "validee"
+        operation.traite_par = 1
+
+        # 4. Sauvegarder
+        session.add(operation)  # signale la modification à SQLModel
+        session.commit()  # envoie le UPDATE à MySQL
+        session.refresh(operation)  # recharge les données depuis MySQL
+
+        return operation
+
+@app.patch("/agent/operations/{id}/refuser")
+def refuser_operation(id: int):
+    with Session(engine) as session:
+
+        operation = session.get(Operation, id)
+
+        if not operation:
+            return {"error": "opération introuvable"}
+
+        operation.statut = "refusee"
+        operation.traite_par = 1
+
+        session.add(operation)
+        session.commit()
+        session.refresh(operation)
+
+        return operation
+#fonctions sur les comptes
+"""""
+option 1 appel au service_auth
+import httpx
+
+@app.get("/agent/clients")
+def get_clients():
+    response = httpx.get("http://service-auth:8001/auth/clients")
+    return response.json()
+"""""
+#option 2 acccède directement à la base de  donné
+@app.get("/agent/clients")
+def get_clients():
+    with Session(engine) as session:
+        clients = session.exec(
+            select(User).where(User.role == "client")
+        ).all()
+        return clients
+@app.get("/agent/clients/{user_id}/comptes")
+def get_compte_client(user_id: int):
+    with Session(engine) as session:
+        comptes= session.exec(select(Compte).where(Compte.user_id == user_id)
+        ).all()
+        if not comptes:
+            return {"error": "compte introuvable"}
+        return comptes
